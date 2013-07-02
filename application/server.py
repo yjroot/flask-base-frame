d1a9983e5ffd577f3ad5ca6307669a761dc9d91f
YeonJun Lim <yjroot@gmail.com>
2013-07-02 16:47:39 +0900

    Added index page.

--- application/server.py
+++ application/server.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 app = Blueprint('main', __name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('main.main'))
 
 @app.route('/main')
 def main():
